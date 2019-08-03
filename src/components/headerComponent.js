f2e6fa89fd60d5c2f9aeaa013823cb8ed52ef8e7
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-08-03 21:46:26 +0530

    Navbar Added

---
+++ src/components/headerComponent.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <div className="container">
+            <NavbarBrand href="/">ExpertEvidence.Org</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">
+                    <span className="fa fa-info fa-lg" /> Help
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">
+                    <span className="fa fa-user-plus fa-lg" /> SignUp
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/">
+                    <span className="fa fa-sign-in fa-lg" /> LogIn
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
